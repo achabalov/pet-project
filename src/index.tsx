ae05685553a02c1387ef3b4463f95da0969cb3d1
achabalov <achabalov@phoenixit.ru>
2023-01-31 20:48:52 +0300

    clean build

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,5 @@
 import {render} from "react-dom";
 import App from './Components/App'
-import './styles/index.scss'
-import {BrowserRouter} from "react-router-dom";
-import {ThemeProvider} from "./theme/ThemeProvides";
 
 const root = document.querySelector('#root')
-render(
-    <BrowserRouter>
-        <ThemeProvider><App/></ThemeProvider>
-    </BrowserRouter>, root)
+render(<App/>, root)
