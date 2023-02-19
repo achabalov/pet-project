23aa129b5454d603db5aa7370d702c9f85c64f9f
achabalov <achabalov@phoenixit.ru>
2023-02-19 18:16:00 +0300

    added CC ErrorBoundary

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,21 +1,18 @@
-import React, {Suspense} from 'react';
-import {Routes, Route} from "react-router-dom";
-import {routeConfig} from "app/router";
+import React, { Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { routeConfig } from 'app/router'
+import { PageLoader } from 'widgets/PageLoader'
 
 function RouteConfig() {
     return (
-        <Suspense fallback={<div>Идет загрузка</div>}>
+        <Suspense fallback={<PageLoader />}>
             <Routes>
-                {Object.values(routeConfig).map(({path, element}) =>
-                    <Route key={path} path={path} element={(
-                        <div className='page-wrapper'>
-                            {element}
-                        </div>
-                    )} />
-                )}
+                {Object.values(routeConfig).map(({ path, element }) => (
+                    <Route key={path} path={path} element={<div className="page-wrapper">{element}</div>} />
+                ))}
             </Routes>
         </Suspense>
-    );
+    )
 }
 
-export default RouteConfig;
+export default RouteConfig
