ce5345ce311ea5196517a46f5dd5986050f98d2d
achabalov <achabalov@phoenixit.ru>
2023-03-20 17:04:26 +0300

    upgrade sidebar, add button themes and size

--- src/widgets/Sidebar/components/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/components/Sidebar/Sidebar.test.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { fireEvent, screen } from '@testing-library/react'
 import { Sidebar } from 'widgets/Sidebar'
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation'
+import { ComponentRender } from 'shared/lib/tests/ComponentRender/ComponentRender'
 
 describe('Sidebar', () => {
     test('', () => {
-        renderWithTranslation(<Sidebar />)
+        ComponentRender(<Sidebar />)
         expect(screen.getByTestId('sidebar')).toBeInTheDocument()
     })
 
     test('', () => {
-        renderWithTranslation(<Sidebar />)
+        ComponentRender(<Sidebar />)
         const toggleBtn = screen.getByTestId('sidebar-toggle')
         expect(screen.getByTestId('sidebar')).toBeInTheDocument()
         fireEvent.click(toggleBtn)
