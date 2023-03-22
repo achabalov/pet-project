4ffb461441b5d146e9708ec8982418b38af54e79
achabalov <achabalov@phoenixit.ru>
2023-03-22 15:31:18 +0300

    added modal window

--- src/widgets/Navbar/components/Navbar.tsx
+++ src/widgets/Navbar/components/Navbar.tsx
@@ -1,16 +1,31 @@
-import React, { VFC } from 'react'
-import { AppLink } from 'shared/ui'
-import { AppLinkTheme } from 'shared/ui/AppLink/AppLink'
-import { useTranslation } from 'react-i18next'
+import React, { useCallback, useState, VFC } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
+import { Modal } from 'shared/ui/Modal/Modal'
+import { useTranslation } from 'react-i18next'
+import { Button, ButtonTheme } from 'shared/ui/Button/Button'
 import styles from './Navbar.module.scss'
 
 interface NavbarProps {
     className?: string
 }
 
-export const Navbar: VFC<NavbarProps> = ({ className }) => (
-    <div className={classNames(styles.Navbar, {}, [styles[className]])}>
-        <div className={styles.link} />
-    </div>
-)
+export const Navbar: VFC<NavbarProps> = ({ className }) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const { t } = useTranslation()
+
+    const onToggleModal = useCallback(() => {
+        setIsOpen(true)
+    }, [])
+
+    return (
+        <div className={classNames(styles.Navbar, {}, [styles[className]])}>
+            <Button theme={ButtonTheme.CLEAR_INVERTED} className={styles.link} onClick={onToggleModal}>
+                {t('Войти')}
+            </Button>
+            <Modal isOpen={isOpen} onClose={setIsOpen}>
+                loremloremloremloremloremloremloremloremloremloremloremloremloremloremlor lore loremlorem loremм loremmem
+            </Modal>
+        </div>
+    )
+}
