3c69cf41d8a53819b92dc00ac5c764f78eb951d5
achabalov <achabalov@phoenixit.ru>
2023-03-23 15:00:05 +0300

    added redux-toolkit to the project

---
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -0,0 +1,22 @@
+import { CounterSchema } from 'entities/Counter'
+import { counterReducer, counterActions } from './counterSlice'
+
+describe('counterSlice.test', () => {
+    test('should increment ', () => {
+        const state: CounterSchema = {
+            value: 10,
+        }
+        expect(counterReducer(state as CounterSchema, counterActions.increment())).toEqual({ value: 11 })
+    })
+
+    test('should decrement', () => {
+        const state: CounterSchema = {
+            value: 10,
+        }
+        expect(counterReducer(state as CounterSchema, counterActions.decrement())).toEqual({ value: 9 })
+    })
+
+    test('should work with empty state', () => {
+        expect(counterReducer(undefined, counterActions.increment())).toEqual({ value: 1 })
+    })
+})
