bcc76aee197261ef843ab0e8a72458a471a7519b
achabalov <achabalov@phoenixit.ru>
2023-03-23 15:00:05 +0300

    added redux-toolkit to the project

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,21 @@ import { render } from 'react-dom'
 import { ErrorBoundary } from 'app/providers/ErrorBoundary'
 import { ThemeProvider } from 'shared/config'
 import { BrowserRouter } from 'react-router-dom'
+import { StoreProvider } from 'app/providers/StoreProvider'
 import App from './app/App'
 import './app/styles/index.scss'
 import 'shared/config/i18n/i18n'
 
 const root = document.querySelector('#root')
 render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>,
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
     root
 )
