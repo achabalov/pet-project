06c3b29f0771c3d6e2145d9902af240d1cd51171
achabalov <achabalov@phoenixit.ru>
2023-03-28 14:08:43 +0300

    added login module

--- src/shared/config/Storybook/RouteDecorator/RouteDecorator.tsx
+++
@@ -1,9 +0,0 @@
-import 'app/styles/index.scss'
-import { Story } from '@storybook/react'
-import { BrowserRouter } from 'react-router-dom'
-
-export const RouteDecorator = (StoryComponent: Story) => (
-    <BrowserRouter>
-        <StoryComponent />
-    </BrowserRouter>
-)
